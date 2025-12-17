782b271a12fbd30c3320dd989c40813f44148229
Colin Miller <cmmiller26@icloud.com>
2025-12-17 13:41:21 -0600

    Lock mouse to center unless DevConsole is open
    Added OnTick to CameraController to set MouseBehavior to LockCenter by default, but allow Default behavior when the developer console is visible. This ensures the mouse is only unlocked when the DevConsole is open.

--- src/client/controllers/CameraController.ts
+++ src/client/controllers/CameraController.ts
@@ -1,16 +1,28 @@
-import { Controller, OnStart } from "@flamework/core";
+import { Controller, OnStart, OnTick } from "@flamework/core";
 import Gizmos from "@rbxts/gizmos";
-import { Players, Workspace } from "@rbxts/services";
+import { Players, StarterGui, UserInputService, Workspace } from "@rbxts/services";
 
 @Controller()
-class CameraController implements OnStart {
+class CameraController implements OnStart, OnTick {
 	readonly camera = Workspace.CurrentCamera!;
 
 	private readonly player = Players.LocalPlayer;
+	private isDevConsoleRegistered = false;
 
 	onStart() {
 		this.player.CameraMode = Enum.CameraMode.LockFirstPerson;
 		//Gizmos.enabled = false;
+	}
+
+	onTick(): void {
+		if (!this.isDevConsoleRegistered) {
+			const [success] = pcall(() => StarterGui.GetCore("DevConsoleVisible"));
+			this.isDevConsoleRegistered = success;
+		}
+		UserInputService.MouseBehavior =
+			this.isDevConsoleRegistered && StarterGui.GetCore("DevConsoleVisible")
+				? Enum.MouseBehavior.Default
+				: Enum.MouseBehavior.LockCenter;
 	}
 }
 
